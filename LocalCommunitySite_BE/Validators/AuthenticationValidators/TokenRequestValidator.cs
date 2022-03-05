b867c4fe48b4dfef9fc50e7440a278329fb6b131
Ya-Vasya <48715786+Ya-Vasya@users.noreply.github.com>
2022-03-05 22:24:16 +0200

    Added refresh token (#1)

---
+++ LocalCommunitySite_BE/Validators/AuthenticationValidators/TokenRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LocalCommunitySite.API.Models.AuthenticationDtos;
+
+namespace LocalCommunitySite.API.Validators.AuthenticationValidators
+{
+    public class TokenRequestValidator : AbstractValidator<TokenRequest>
+    {
+        public TokenRequestValidator()
+        {
+            RuleFor(x => x.Token).NotEmpty();
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
