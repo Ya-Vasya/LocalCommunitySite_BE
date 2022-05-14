c4d11514ec72bd15762ff93935eb3f654df28145
ya-vasya <vasyl.stashchuk.pz.2018@lpnu.ua>
2022-05-14 11:37:26 +0300

    Added migtrations

--- LocalCommunitySite.Infrastructure/Migrations/20220502114752_AddedPosts.Designer.cs
+++ LocalCommunitySite.Infrastructure/Migrations/20220502114752_AddedPosts.Designer.cs
@@ -4,14 +4,16 @@ using LocalCommunitySite.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LocalCommunitySite.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220502114752_AddedPosts")]
+    partial class AddedPosts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
