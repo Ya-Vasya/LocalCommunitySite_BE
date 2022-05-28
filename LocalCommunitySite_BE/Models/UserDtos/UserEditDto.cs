4023df08f2df151aaef4f51038071fdefa2d74fe
ya-vasya <vasyl.stashchuk.pz.2018@lpnu.ua>
2022-05-28 17:43:12 +0300

    Added user dtos

---
+++ LocalCommunitySite_BE/Models/UserDtos/UserEditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalCommunitySite.API.Models.UserDtos
+{
+    public class UserEditDto
+    {
+        public string Email { get; set; }
+
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
